306552bd29553b4403572082d7b51e51a5c4e31d
Julia-Skliarenko <rygaja15@gmail.com>
2025-03-09 14:34:30 -0500

    2V

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,7 +3,7 @@ import css from "../Description/Description.module.css"
 export default function Description() {
     return (
         <div className={css.containerDescription}>
-            <h1>Sip Happens Café</h1>;
+            <h1>Sip Happens Café</h1>
             <p>
                 Please leave your feedback about our service by selecting one of the options below.
             </p>
